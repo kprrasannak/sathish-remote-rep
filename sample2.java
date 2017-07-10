5a497f083fbf43a3c7a1b74b344dcf66629f9a46
ASK <ASK@gmail.com>
2017-07-10 10:58:25 -0700

    sample2

---
+++ sample2.java
@@ -0,0 +1,14 @@
+public class Sample{
+
+   public static void main(String args[]){
+      int a=20, b=30;
+
+      if(b>a)
+         System.out.println("b is greater");
+	
+	else
+       
+        System.out.println("a is greater"); 
+      }
+}
+}
